0bc64a3436e0439fd6d112c447ab535bd9351526
eric-gns <genosamarkeric@gmail.com>
2026-08-24 06:28:09 +0800

    Initial commit - Day 1 to Day 3 solutions

---
+++ day_1.py
@@ -0,0 +1,20 @@
+print("Hello, World!")
+
+print(1+2)
+print(2-3)
+print(2*6)
+print(6/2)
+print(2**2)
+print(3%2)
+print(2//3)
+
+print(type(10))
+print(type(3.14))
+print(type(1 + 3j))
+# ``j`` denotes the imaginary part of a complex number; ``e`` is used for
+# scientific notation and must include an exponent, such as 3e2 (300.0).
+print(3e2)
+print(type('hey hey hey'))
+print(type([1, 2, 3, 4, 5]))
+print(type({'name': 'Eric', 'age': 30}))
+print(type({9.1, 6.7, 1.1, 3.4 }))
